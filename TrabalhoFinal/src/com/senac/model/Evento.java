40d6d517a7ec72863c2289ba388149d259fef9f2
Guilherme Soster <guilhermesosters@gmail.com>
2014-06-08 22:29:33 -0300

    Adicionando model.Evento e model.Usuario

--- TrabalhoFinal/src/com/senac/model/Evento.java
+++ TrabalhoFinal/src/com/senac/model/Evento.java
@@ -1,5 +1,52 @@
 package com.senac.model;
 
+/**
+ * Classe que representa um Evento (Cadastro de Issue, 
+ * atualizacao de situação, etc). 
+ * Como funciona apenas como repositório de informações
+ * não contém Testes.
+ */
 public class Evento {
 
+	private int dataCriacao;
+	private String comentario;
+	private Usuario usuario;
+	
+	/**
+	 * Construtor da classe Evento. Quando um novo evento for
+	 * ser criado, será necessário que o mesmo receba a data em que foi criado
+	 * o comentario do evento e o usuario que criou o evento
+	 * obs.: A data/hora será fornecida automaticamente (controller) conforme
+	 * a hora atual do sistema. 
+	 * @param dataCriacao
+	 * @param comentario
+	 * @param usuario
+	 */
+	public Evento(int dataCriacao, String comentario, Usuario usuario){
+		this.setDataCriacao(dataCriacao);
+		this.setComentario(comentario);
+		this.setUsuario(usuario);
+	}
+	
+	//Metodos Acessores
+	public int getDataCriacao() {
+		return dataCriacao;
+	}
+	public void setDataCriacao(int dataCriacao) {
+		this.dataCriacao = dataCriacao;
+	}
+	public String getComentario() {
+		return comentario;
+	}
+	public void setComentario(String comentario) {
+		this.comentario = comentario;
+	}
+	public Usuario getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+	
+	
 }
--- TrabalhoFinal/src/com/senac/model/Usuario.java
+++ TrabalhoFinal/src/com/senac/model/Usuario.java
@@ -1,5 +1,55 @@
 package com.senac.model;
 
+/**
+ * Classe representa um usuario do sistema.
+ * Como funciona apenas como repositório de informações
+ * não contém Testes.
+ */
 public class Usuario {
 
+	private String nome;
+	private String username;
+	private String senha;
+	
+	/**
+	 * Construtor da classe Usuario. Quando um novo usuario for 
+	 * ser instanciado é necessário que se informe um nome, username
+	 * e senha.
+	 * @param nome
+	 * @param username
+	 * @param senha
+	 */
+	public Usuario(String nome, String username, String senha){
+		this.setNome(nome);
+		this.setUsername(username);
+		this.setSenha(senha);
+	}
+
+	//Abaixo métodos acessores.
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+	
+	
 }
