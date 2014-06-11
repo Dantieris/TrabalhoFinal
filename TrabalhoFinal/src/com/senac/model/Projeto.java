5026729657eca78bceb30f8a9c4d0f19dd2d1c8e
Guilherme Soster <guilhermesosters@gmail.com>
2014-06-11 09:22:40 -0300

    Adicionando Mockito.jar as pastas do projeto para facilitar a utilizacao pelos outros membros. Iniciando Unit tests da classe Projeto

--- TrabalhoFinal/src/com/senac/model/Projeto.java
+++ TrabalhoFinal/src/com/senac/model/Projeto.java
@@ -1,5 +1,54 @@
 package com.senac.model;
 
-public class Projeto {
+import com.senac.model.Usuario;
+import com.senac.lib.Vetor;
+import com.senac.lib.ListaOrdenada;
 
+public class Projeto implements Comparable<Projeto> {
+	
+	private Usuario criador;
+	private Vetor<Usuario> desenvolvedor;
+	private String nome;
+	private String descricao;
+ 	private ListaOrdenada<Issue> issue;
+	
+ 	
+ 	public Projeto(Usuario criador, String nome, String descricao){
+ 		this.setCriador(criador);
+ 		this.setNome(nome);
+ 		this.setDescricao(descricao);
+ 	}
+ 	
+	public int compareTo(Projeto proj){
+		return 0;
+	}
+
+	public Usuario getCriador() {
+		return criador;
+	}
+
+	public void setCriador(Usuario criador) {
+		this.criador = criador;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	
+	
+	
+	
 }
--- TrabalhoFinal/src/com/senac/tests/model/ProjetoTest.java
+++ TrabalhoFinal/src/com/senac/tests/model/ProjetoTest.java
@@ -5,15 +5,21 @@ import static org.junit.Assert.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-
+import com.senac.model.Projeto;
+import com.senac.model.Usuario;
+import static org.mockito.Mockito.*;
 public class ProjetoTest {
-
+	Projeto projeto;
+	Usuario criador;
 	@Before
 	public void setUp() throws Exception {
+		this.criador = mock(Usuario.class);
+		projeto = new Projeto(criador, "Projeto Escola", "Projeto de uma escola totalmente online");
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		projeto = null;
 	}
 
 	@Test
